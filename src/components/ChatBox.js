73d20fcd8d54f6dadb1ed26c40fd14b34d330079
lixuling <antonisaputra049@gmail.com>
2022-10-08 13:08:13 +0700

    cok 5

--- src/components/ChatBox.js
+++ src/components/ChatBox.js
@@ -34,12 +34,7 @@ export default function ChatBox({ dis, setChatBox }) {
   } = ChatState();
 
   useEffect(() => {
-    socket = io(ENDPOINT, {
-      withCredentials: true,
-      extraHeaders: {
-    "my-custom-header": "abcd"
-  }
-  });
+    socket = io(ENDPOINT);
     socket.emit("setup", JSON.parse(localStorage.getItem("user")));
     socket.on("connected", () => setConnetedSocket(true));
     socket.on("typing", () => setIsTyping(true));
