c113de27517537ccdff695c73e491ae2d67106a4
lixuling <antonisaputra049@gmail.com>
2022-10-08 21:07:42 +0700

    fetch

--- src/components/ChatBox.js
+++ src/components/ChatBox.js
@@ -91,6 +91,7 @@ export default function ChatBox({ dis, setChatBox }) {
           config
         );
         socket.emit("new message", data);
+        setFetchAgain(!fetchAgain)
         setMessages([...messages, data]);
       } catch (error) {
         throw new Error(error);
